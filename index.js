4198578bac110e0d9f5748c4fe0f355a87595d6d
willber10 <willberschickler@gamil.com>
2024-01-27 15:17:56 -0800

    started to add inquirer promts to the index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquirer");
+
+const mysql = require("mysql");
+
+function init () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: ['View all employees', 'View all departments', 'View all roles', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Exit']
+        }
+    ]).then((answers) => {
+        switch (answers.options) {
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Add department':
+                addDepartment();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Update employee role':
+                updateRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+}
+
+init();
