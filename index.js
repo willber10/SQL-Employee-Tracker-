857ccf2f00b59ac37689b7fa3e7c4543d92efedf
willber10 <willberschickler@gamil.com>
2024-01-27 15:41:13 -0800

    installed mysql2 and updated index .js

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mewtwo',
+    database: 'employee_tracker_db'
+});
 
-function init () {
-    inquirer.prompt([
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(results);
+        }
+    })
+};
+
+function viewDepartments() {
+    console.log("View Departments");
+}
+
+function viewRoles() {
+    console.log("View Roles");
+}
+
+function addEmployee() {
+    console.log("Add Employee");
+}
+
+function addDepartment() {
+    console.log("Add Department");
+}
+
+function addRole() {
+    console.log("Add Role");
+}
+
+function updateRole() {
+    console.log("Update Role");
+}
+
+// RESULT: initializes the application
+function init() {
+    inquirer.prompt(
         {
             type: 'list',
-            name: 'options',
             message: 'What would you like to do?',
+            name: 'options',
             choices: ['View all employees', 'View all departments', 'View all roles', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Exit']
-        }
-    ]).then((answers) => {
+        },
+    )
+    .then((answers) => {
         switch (answers.options) {
             case 'View all employees':
                 viewEmployees();
